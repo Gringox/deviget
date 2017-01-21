fa8753dc2a34ba64fb81942b457f0c9f5b6cd2d1
Pedro Luis Valdivieso <pedro.valdivieso@gmail.com>
2017-01-21 15:33:56 -0400

    Added project structure

---
+++ deviget/RedditListView.swift
@@ -0,0 +1,17 @@
+//
+//  RedditListView.swift
+//  deviget
+//
+//  Created by Pedro Luis Valdivieso on 1/21/17.
+//  Copyright © 2017 Pedro Luis Valdivieso. All rights reserved.
+//
+
+import UIKit
+
+class RedditListView: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+
+}
